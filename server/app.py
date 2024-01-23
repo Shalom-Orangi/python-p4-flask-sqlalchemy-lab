9cdba2636836ee4fa95a241285b8cba1fa3948b2
Shalom Orangi <shalom.orangi@student.moringaschool.com>
2024-01-23 23:07:40 +0300

    Update

--- server/app.py
+++ server/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import Flask, make_response
+from flask import Flask, make_response,render_template
 from flask_migrate import Migrate
 
 from models import db, Zookeeper, Enclosure, Animal
@@ -19,15 +19,42 @@ def home():
 
 @app.route('/animal/<int:id>')
 def animal_by_id(id):
-    return ''
+    animal=Animal.query.get(id)
+    animal_data={
+        'name':animal.name,
+        'species':animal.species,
+        'zookeeper':animal.zookeeper.name if animal.zookeeper else None,
+        'enclosure':animal.enclosure.environment if animal.enclosure else None
+    }
+    response=make_response(render_template(+'animal.html',animal_data=animal_data))
+
+    return response
 
 @app.route('/zookeeper/<int:id>')
 def zookeeper_by_id(id):
-    return ''
+    zookeeper=Zookeeper.query.get(id)
+    zookeeper_data = {
+        'name': zookeeper.name,
+        'birthday': zookeeper.birthday,
+        'animals': [animal.name for animal in zookeeper.animals]
+    }
+    response=make_response(render_template('zookeeper.html', zookeeper_data=zookeeper_data))
+    response.headers['Content-Type'] = 'text/html'
 
+    return response
+    
 @app.route('/enclosure/<int:id>')
 def enclosure_by_id(id):
-    return ''
+    enclosure=Enclosure.query.get(id)
+    enclosure_data={
+        'environment': enclosure.environment,
+        'open_to_visitors': enclosure.open_to_visitors,
+        'animals': [animal.name for animal in enclosure.animals]
+    }
+    response=make_response(render_template('enclosure.html',enclosure_data=enclosure_data))
+    response.headers['Content-Type']='text/html'
+
+    return response
 
 
 if __name__ == '__main__':
--- server/models.py
+++ server/models.py
@@ -11,13 +11,41 @@ class Zookeeper(db.Model):
     __tablename__ = 'zookeepers'
 
     id = db.Column(db.Integer, primary_key=True)
+    name=db.Column(db.String)
+    birthday=db.Column(db.Date,nullable=False)
 
+    #relationship
+    animals=db.relationship('Animal',backref='zookeeper')
+    def __repr__(self):
+        return f'<Animal{self.name},{self.birthday}>'
+    
 class Enclosure(db.Model):
     __tablename__ = 'enclosures'
 
     id = db.Column(db.Integer, primary_key=True)
+    environment=db.column(db.String(255),nullable=False)
+    open_to_visitors=db.Column(db.Boolean,default=True)
 
+    #relationship
+    animals=db.relationship('Animal',backref='enclosure')
+
+    def __repr__(self):
+        return f'<{self.environment},{self.open_to_visitors}>'
+    
 class Animal(db.Model):
     __tablename__ = 'animals'
 
     id = db.Column(db.Integer, primary_key=True)
+    name=db.Column(db.String)
+    species=db.Column(db.String)
+
+    #foreign keys
+    zookeeper_id=db.Column(db.Integer,db.ForeignKey('zookeepers.id'))
+    enclosure_id=db.Column(db.Integer,db.ForeignKey('enclosures.id'))
+
+    #relationships
+    enclosures=db.relationship('Enclosure',backref='animal')
+    zookeepers=db.relationship('Zookeeper',backref='animal')
+
+    def __repr__(self):
+        return f'<Animal{self.name},{self.species},{self.zookeeper_id},{self.enclosure_id}>'
